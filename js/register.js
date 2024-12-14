3a002129527821ad71efe0f46ae524f369d3e4e2
StivenDelgado <delgadostiven123@gmail.com>
2024-12-13 20:24:05 -0500

    registro hecho

---
+++ js/register.js
@@ -0,0 +1,33 @@
+class Register {
+    constructor() {
+        this.form = document.querySelector("form");
+        this.form.addEventListener("submit", this.submitHandler.bind(this));
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+        const objeto = new FormData(this.form);
+        const usuario = Object.fromEntries(objeto.entries());
+        if (usuario.password !== usuario.confirmar) {
+            alert("Las contraseñas no coinciden");
+            return;
+        }
+        if (usuario.password.length < 8) {
+            alert("La contraseña debe tener al menos 8 caracteres");
+            return;
+        }
+        alert("Registro exitoso");
+        if (localStorage.getItem("usuarios")) {
+            const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            usuarios.push(usuario);
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        }else{
+            const usuarios = [usuario];
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        }
+
+        window.location.href = "login.html";
+    }
+}
+
+const register = new Register();
