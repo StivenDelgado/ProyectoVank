5a4cd00d7ae5e3ffaad47a8f266140ec7b190981
StivenDelgado <delgadostiven123@gmail.com>
2024-12-21 13:55:57 -0500

    arreglo index

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 //verifica que existe un usuario en localStore
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
+
+    if (!localStorage.getItem('usuarioLogueado')) {
+        alert('Debes estar logeado navegar en la página');
+        window.location.href = 'login.html';
+        return
+    }
     const usuario = localStorage.getItem("username");
 
     //Se obtiene el elemento del saludo
     const saludo = document.querySelector("main section h3");
 
-    if(usuario){
+    if (usuario) {
         //muestra el saludo con el nombre
         saludo.innerHTML = `Hola, <strong>${usuario}</strong>. <br> Tu saldo es de <span style="color: green;">2.500.000 COP</span>.`;
     }
